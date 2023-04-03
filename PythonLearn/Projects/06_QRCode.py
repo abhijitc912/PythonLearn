e4d08ca5578c30eadd06ca1a886ed306dfb09d8f
Abhijit CHAKRABORTY <abhijit.chakraborty@amadeus.com>
2023-04-03 11:25:04 +0530

    Update 3Apr2023

--- PythonLearn/Projects/06_QRCode.py
+++ PythonLearn/Projects/06_QRCode.py
@@ -8,4 +8,4 @@ s = "https://www.facebook.com/abhijit.chakraborty.0912"
 url = pyqrcode.create(s) 
   
 # Create and save the png file naming "myqr.png" 
-url.svg("qrcode.svg", scale = 8) 
+url.svg("Projects/qrcode.svg", scale = 8) 
